31f620b79c58d9e34c4d4ebeb00da1d66b19a9a5
Prithivian <113180946+Prithivian@users.noreply.github.com>
2022-09-10 10:30:03 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+burger = document.querySelector('burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.rightnav')
+burger.addEventListener('click', () => {
+        rightnav.classList.toggle('v-class-resp')
+        navlist.classList.toggle('v-class-resp')
+        navbar.classList.toggle('h-nav-resp')
+     })
