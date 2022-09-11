f9332ac6fd9dcb73d8e1efd4ab05612536b6ae86
Prithivian <113180946+Prithivian@users.noreply.github.com>
2022-09-11 00:14:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ navbar = document.querySelector('.navbar')
 rightnav = document.querySelector('.rightnav')
 burger.addEventListener('click', () => {
         rightnav.classList.toggle('v-class-resp')
-        navlist.classList.toggle('v-class-resp')
+        nav-list.classList.toggle('v-class-resp')
         navbar.classList.toggle('h-nav-resp')
      })
